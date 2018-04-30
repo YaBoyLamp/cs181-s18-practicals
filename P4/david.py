6ef652cd6eac6dffa31ee4aba56577944403f095
David Gibson <davidgibson@college.harvard.edu>
2018-04-29 23:36:18 -0400

    current david

--- P4/david.py
+++ P4/david.py
@@ -23,6 +23,7 @@ class Learner(object):
         self.is_low_gravity = None
         self.count = 0
         self.epochs = 1
+        self.gravitys = []
 
     def reset(self):
         self.last_state  = None
@@ -52,10 +53,11 @@ class Learner(object):
 
         if self.is_low_gravity == None:
             self.is_low_gravity = state['monkey']['vel'] == -1
+            self.gravitys.append(state['monkey']['vel'] == -1)
 
-        ls = (self.last_state['tree']['dist'] / 50, self.last_state['tree']['top'] / 50, self.last_state['monkey']['vel'] / 10, self.last_state['monkey']['top'] / 50, self.is_low_gravity)
-        s = (state['tree']['dist'] / 50, state['tree']['top'] / 50, state['monkey']['vel'] / 10, state['monkey']['top'] / 50, self.is_low_gravity)
-        self.q[(ls, self.last_action)] = ((1 - self.alpha) * self.q[(ls, self.last_action)]) + (self.alpha * ((self.last_reward + 0.01) + self.gamma * max([self.q[(s,0)], self.q[(s,1)]])))
+        ls = (self.last_state['tree']['dist'] / 30, self.last_state['tree']['top'] / 30, self.last_state['monkey']['vel'] / 10, self.last_state['monkey']['top'] / 30, self.is_low_gravity)
+        s = (state['tree']['dist'] / 30, state['tree']['top'] / 30, state['monkey']['vel'] / 10, state['monkey']['top'] / 30, self.is_low_gravity)
+        self.q[(ls, self.last_action)] = ((1 - self.alpha) * self.q[(ls, self.last_action)]) + (self.alpha * ((self.last_reward) + (self.gamma * max([self.q[(s,0)], self.q[(s,1)]]))))
 
         if state['monkey']['top'] + state['monkey']['vel'] < 0:
             self.last_action = 1
@@ -135,7 +137,7 @@ if __name__ == '__main__':
     hist = []
 
     # Run games. 
-    run_games(agent, hist, 1000, 0)
+    run_games(agent, hist, 2000, 0)
 
     # Save history. 
     np.save('hist',np.array(hist))
@@ -153,17 +155,18 @@ if __name__ == '__main__':
     # print 'max treemid = ', max(agent.treemids)
     # print 'max monkeyvel = ', max(agent.monkeyvels)
     # print 'min monkeyvel = ', min(agent.monkeyvels)
-    print sum([x != 0 for x in agent.q.values()])
-    print len(agent.q)
+    # print sum([x != 0 for x in agent.q.values()])
+    # print len(agent.q)
     print (agent.count)
     print max(hist)
+    print sum(agent.gravitys)
     # print agent.q.values()
 
-    # with open('qs.txt', 'w') as f:
-    #     for key, value in agent.q.items():
-    #         f.write('%s:%s\n' % (key, value))
+    with open('scores2.txt', 'w') as f:
+        for score in xrange(2000):
+            f.write('%s:%s:%r\n' % (score, hist[score], agent.gravitys[score]))
 
-    plt.plot(range(1,1001), hist)
+    plt.plot(range(1,2001), hist)
     plt.show()
 
     plt.hist(hist)
