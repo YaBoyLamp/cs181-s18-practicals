7e4a5beb446453605c46291658e54eadbd9ae0db
David Gibson <davidgibson@college.harvard.edu>
2018-05-02 02:55:58 -0400

    final david code

--- P4/david.py
+++ P4/david.py
@@ -23,6 +23,7 @@ class Learner(object):
         self.is_low_gravity = None
         self.count = 0
         self.epochs = 1
+        self.gravitys = []
 
     def reset(self):
         self.last_state  = None
@@ -45,20 +46,23 @@ class Learner(object):
         self.count += 1
         new_action = 0
 
+        # swing on the first epoch
         if self.last_state == None:
             self.last_action = 0
             self.last_state = state
             return 0
         
-
+        # infer gravity
         if self.is_low_gravity == None:
             self.is_low_gravity = state['monkey']['vel'] == -1
             self.gravitys.append(state['monkey']['vel'] == -1)
 
-        ls = (self.last_state['tree']['dist'] / 60, self.last_state['tree']['top'] / 60, self.last_state['monkey']['vel'] / 10, self.last_state['monkey']['top'] / 60, self.is_low_gravity)
-        s = (state['tree']['dist'] / 60, state['tree']['top'] / 60, state['monkey']['vel'] / 10, state['monkey']['top'] / 60, self.is_low_gravity)
+        # transform states into our specific state representation and update q value for the last state action pair
+        ls = (self.last_state['tree']['dist'] / 70, self.last_state['tree']['top'] / 70, self.last_state['monkey']['vel'] / 10, self.last_state['monkey']['top'] / 70, self.is_low_gravity)
+        s = (state['tree']['dist'] / 70, state['tree']['top'] / 70, state['monkey']['vel'] / 10, state['monkey']['top'] / 70, self.is_low_gravity)
         self.q[(ls, self.last_action)] = ((1 - self.alpha) * self.q[(ls, self.last_action)]) + (self.alpha * ((self.last_reward) + (self.gamma * max([self.q[(s,0)], self.q[(s,1)]]))))
 
+        # always jump if the monkey is about to fall off the bottom and always swing if the monkey is about to rise off the top
         if state['monkey']['top'] + state['monkey']['vel'] < 0:
             self.last_action = 1
             self.last_state = state 
@@ -68,6 +72,7 @@ class Learner(object):
             self.last_state = state
             return 0
 
+        # change hyperparameters based on a schedule
         if self.epochs > 250:
             self.epsilon = 0.05
         if self.epochs > 500:
@@ -79,25 +84,17 @@ class Learner(object):
         if self.epochs > 750:
             self.alpha = 0.1
 
+
+        # epsilon greedy exploration vs explotation decision
         if npr.rand() < self.epsilon:
             new_action = 1 if npr.rand() < 0.1 else 0
         else:
             if self.q[(s, 0)] > self.q[(s, 1)]:
                 new_action = 0
-                # self.count += 1
             elif self.q[(s, 0)] < self.q[(s, 1)]:
                 new_action = 1
-                # self.count += 1
-            else:
+            else: # choose action randomly if q values for jumping and swinging are equal
                 new_action = 1 if npr.rand() < 0.1 else 0
-            # else:
-            #     if state['tree']['dist'] > 0 and state['monkey']['vel'] < -20 and self.last_action != 2:
-            #         if state['monkey']['top'] < (state['tree']['top'] + state['tree']['bot'] / 2):
-            #             new_action = 1
-            #         else:
-            #             new_action = 0
-            #     else:
-            #         new_action = 1 if npr.rand() < 0.1 else 0
 
         new_state  = state
 
@@ -151,30 +148,51 @@ if __name__ == '__main__':
     # Save history. 
     np.save('hist',np.array(hist))
 
-    # print 'treegaps = ', agent.treegaps
-    # print 'treetops = ', agent.treetops
-    # print 'treegaps = ', agent.treebots
-    # print 'treemids = ', agent.treemids
+    lowgravcount = 0
+    highgravcount = 0
+    lowgravsum = 0
+    highgravsum = 0
+    for trial in range(899,1000):
+        if agent.gravitys[trial]:
+            lowgravcount += 1
+            lowgravsum += hist[trial]
+        else:
+            highgravcount += 1
+            highgravsum += hist[trial]
 
-    # print 'max treetop = ', max(agent.treetops)
-    # print 'min treetop = ', min(agent.treetops)
-    # print 'max treebot = ', max(agent.treebots)
-    # print 'min treebot = ', min(agent.treebots)
-    # print 'min treemid = ', min(agent.treemids)
-    # print 'max treemid = ', max(agent.treemids)
-    # print 'max monkeyvel = ', max(agent.monkeyvels)
-    # print 'min monkeyvel = ', min(agent.monkeyvels)
-    # print sum([x != 0 for x in agent.q.values()])
-    # print len(agent.q)
-    print (agent.count)
-    print max(hist)
-    print len(agent.q)
+    print 'high gravity average score for epochs 900-1000:', float(highgravsum) / highgravcount
+    print 'low gravity average score for epochs 900-1000:', float(lowgravsum) / lowgravcount
 
-    with open('scores2.txt', 'w') as f:
-        for score in xrange(1000):
-            f.write('%s:%s:%r\n' % (score, hist[score], agent.gravitys[score]))
+    lowgravavgs = []
+    highgravavgs = []
+    for bin in range(0,10):
+        lowgravsum = 0
+        lowgravcount = 0
+        highgravsum = 0
+        highgravcount = 0
+        for trial in range(0,100):
+            if agent.gravitys[bin * 100 + trial]:
+                lowgravcount += 1
+                lowgravsum += hist[bin * 100 + trial]
+            else:
+                highgravcount += 1
+                highgravsum += hist[bin * 100 + trial]
+        lowgravavgs.append(float(lowgravsum) / lowgravcount)
+        highgravavgs.append(float(highgravsum) / highgravcount)
 
-    plt.plot(range(1,1001), hist)
+    plt.figure(1)
+    plt.bar(range(0, 10), lowgravavgs)
+    plt.xticks(range(0,10), ('1-100', '101-200', '201-300', '301-400', '401-500', '501-600', '601-700', '701-800', '801-900', '901-1000'))
+    plt.xlabel('Epoch')
+    plt.ylabel('Average Score')
+    plt.title('Low Gravity Average Score per 100 Epochs')
+
+    plt.figure(2)
+    plt.bar(range(0, 10), highgravavgs)
+    plt.xticks(range(0,10), ('1-100', '101-200', '201-300', '301-400', '401-500', '501-600', '601-700', '701-800', '801-900', '901-1000'))
+    plt.xlabel('Epoch')
+    plt.ylabel('Average Score')
+    plt.title('High Gravity Average Score per 100 Epochs')
+
     plt.show()
 
-    plt.hist(hist)
